0c237387e9464cf4aff77904eb6827c39106e775
Tina Shumilova <shutiva@gmail.com>
2022-07-25 16:26:06 +0300

    Activity_2
    Second activity

---
+++ TA_Bootcamp_Java_2022/src/com/bootcampexcercise/module2/activity/OperatorActivity.java
@@ -0,0 +1,25 @@
+package com.bootcampexcercise.module2.activity;
+
+/**
+ *
+ *     Perform the following:
+ *     Find the sum of the given integers
+ *     Print the result
+ *
+ */
+
+public class OperatorActivity {
+    public static void main(String[] args) {
+        int a = 56;
+        int b = 134;
+
+        int sum = a + b;
+
+        System.out.println(sum);
+
+    }
+
+
+
+
+}
