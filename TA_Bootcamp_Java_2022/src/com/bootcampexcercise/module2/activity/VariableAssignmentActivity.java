961cba9d2890ab8c441f4426342d5fd46e6964d4
Tina Shumilova <shutiva@gmail.com>
2022-07-25 14:37:43 +0300

    First_activity

---
+++ TA_Bootcamp_Java_2022/src/com/bootcampexcercise/module2/activity/VariableAssignmentActivity.java
@@ -0,0 +1,17 @@
+package com.bootcampexcercise.module2.activity;
+
+public class VariableAssignmentActivity {
+
+    public static void main(String[] args) {
+        int a = 5;
+        a = 7;
+        System.out.println("Result = " + a);
+    }
+
+
+
+    /*Perform the following:
+    Declare a variable of type int and assign it default value
+    Update the value
+    Print the updated value to the console*/
+}
