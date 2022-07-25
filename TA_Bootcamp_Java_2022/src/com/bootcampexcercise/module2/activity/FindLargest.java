9949deef93d960fbfd4fd9de32a474384bb0e7a0
Tina Shumilova <shutiva@gmail.com>
2022-07-25 16:55:58 +0300

    Activity_3

---
+++ TA_Bootcamp_Java_2022/src/com/bootcampexcercise/module2/activity/FindLargest.java
@@ -0,0 +1,16 @@
+package com.bootcampexcercise.module2.activity;
+
+public class FindLargest {
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 10;
+        int max = a;
+        if (b > max){
+            max = b;
+            System.out.println(max);
+        }else {
+            System.out.println(max);
+        }
+
+    }
+}
