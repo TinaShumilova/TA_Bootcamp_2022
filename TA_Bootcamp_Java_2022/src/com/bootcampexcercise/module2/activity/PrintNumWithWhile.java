6d4a6357f8cf4cd26648a4109e7f78ee2e85d858
Tina Shumilova <shutiva@gmail.com>
2022-07-26 10:02:22 +0300

    Activity_7

---
+++ TA_Bootcamp_Java_2022/src/com/bootcampexcercise/module2/activity/PrintNumWithWhile.java
@@ -0,0 +1,12 @@
+package com.bootcampexcercise.module2.activity;
+
+public class PrintNumWithWhile {
+    public static void main(String[] args) {
+        int x =1;
+        while (x<100){
+                System.out.println("The value of x is " + x);
+                x = x + 2;
+
+        }
+    }
+}
