41f68ccbdd1420b5e8e9d2d6b223edda39be1462
Tina Shumilova <shutiva@gmail.com>
2022-07-26 14:18:18 +0300

    Activity1

---
+++ TA_Bootcamp_Java_2022/src/com/bootcampexcercise/module3/activity/DateActivity.java
@@ -0,0 +1,20 @@
+package com.bootcampexcercise.module3.activity;
+
+import java.util.Date;
+
+/**
+ * Create a java file DateActivity.java inside the package sef.module4.activity
+ * Import java.util.Date
+ * Create instance d of Date using Date()
+ * Print today’s date
+ */
+
+
+public class DateActivity {
+    public static void main(String[] args) {
+
+        Date todayDate = new Date();
+        //print the date
+        System.out.println(todayDate);
+    }
+}
