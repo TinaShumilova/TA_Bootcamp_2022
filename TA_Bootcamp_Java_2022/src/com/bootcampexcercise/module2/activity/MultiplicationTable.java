104e4999f8769c480d06edb18d2bbfdcfb7677c3
Tina Shumilova <shutiva@gmail.com>
2022-07-26 09:39:31 +0300

    Activity_6

---
+++ TA_Bootcamp_Java_2022/src/com/bootcampexcercise/module2/activity/MultiplicationTable.java
@@ -0,0 +1,16 @@
+package com.bootcampexcercise.module2.activity;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+
+        for (int i = 11; i <= 20 ; i++) {
+            System.out.println("*** Table of " + i + "***");
+            for (int j = 1; j <= 10; j++) {
+                System.out.println(i + "x" + j + " = " + (i*j));
+            }
+        }
+
+
+
+    }
+}
