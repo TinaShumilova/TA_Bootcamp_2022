eebc4e75b4a8d5770fdfc8c1730dbd952364e5b1
Tina Shumilova <shutiva@gmail.com>
2022-07-26 11:43:43 +0300

    Activity_9

---
+++ TA_Bootcamp_Java_2022/src/com/bootcampexcercise/module2/activity/NumToWordsUsingMethod.java
@@ -0,0 +1,16 @@
+package com.bootcampexcercise.module2.activity;
+
+/**
+ *Take a look at NumToWords.java in which we wrote code to convert integer numbers into their text value
+ * NumToWordsUsingMethod.java will perform the same function using method printWord()
+ * Complete the code in NumToWordsUsingMethod.java
+ * */
+
+
+public class NumToWordsUsingMethod {
+
+    public static void main(String[] args) {
+        NumToWords collingTaskFive = new NumToWords();
+        collingTaskFive.numberInLetter(5);
+    }
+}
