5ca0552186bdaf21654469304a0e114579cee669
Tina Shumilova <shutiva@gmail.com>
2022-07-27 11:24:05 +0300

    updates

--- TA_Bootcamp_Java_2022/src/com/bootcampexcercise/module5/activity/Person_I.java
+++ TA_Bootcamp_Java_2022/src/com/bootcampexcercise/module5/activity/Person_I.java
@@ -6,7 +6,6 @@ public class Person_I {
     private int age;
 
     //Behavior
-    //TODO: write default constructor. Print 'I'm Person_I constructor'
     public Person_I() {
         this.name = "Unknown";
         this.age = 0;
@@ -14,29 +13,23 @@ public class Person_I {
 
     }
 
-    //TODO: Write parameterized constructor with variables name and age
     public Person_I(String name, int age) {
         this.name = name;
         this.age = age;
     }
 
-
-    //TODO: write getter for String name
     public String getName() {
         return name;
     }
 
-    //TODO: write setter for String name
     public void setName(String name) {
         this.name = name;
     }
 
-    //TODO: write getter for int age
     public int getAge() {
         return age;
     }
 
-    //TODO: write setter for int age
     public void setAge(int age) {
         this.age = age;
     }
