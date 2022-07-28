0b6526c3666b92f6e9686506962fc344a5bd9774
Tina Shumilova <shutiva@gmail.com>
2022-07-28 13:56:27 +0300

    Update ListActivity.java

--- TA_Bootcamp_Java_2022/src/com/bootcampexcercise/module8/activity/ListActivity.java
+++ TA_Bootcamp_Java_2022/src/com/bootcampexcercise/module8/activity/ListActivity.java
@@ -6,13 +6,13 @@ import java.util.List;
 
 public class ListActivity {
     public static void main(String[] args) {
-        //TODO: 1 - Type code to create a list of names. Use ArrayList.
+
         List myList = new ArrayList();
         myList.add("Tina");
         myList.add("John");
         myList.add(0, "Viktor");
 
-        //TODO: 2 - Call print method to print the list passed as its parameter.
+
         ListActivity obj = new ListActivity();
         obj.printList(myList);
     }
@@ -20,7 +20,7 @@ public class ListActivity {
     void printList(List list){
 
         Iterator<Object> myIterator = list.iterator();
-        System.out.println("List of Names");
+        System.out.println("***List of Names***");
         while (myIterator.hasNext()){
             System.out.println(myIterator.next());
         }
