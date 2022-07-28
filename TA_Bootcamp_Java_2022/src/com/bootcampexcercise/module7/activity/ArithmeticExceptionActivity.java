5e4c4dddf19e56fe00bb7628eb8f716dd12a83a6
Tina Shumilova <shutiva@gmail.com>
2022-07-28 10:13:59 +0300

    Module7_Activity2

---
+++ TA_Bootcamp_Java_2022/src/com/bootcampexcercise/module7/activity/ArithmeticExceptionActivity.java
@@ -0,0 +1,23 @@
+package com.bootcampexcercise.module7.activity;
+
+public class ArithmeticExceptionActivity {
+
+    public static void main(String[] args) {
+
+        ArithmeticExceptionActivity obj = new ArithmeticExceptionActivity();
+        obj.catchMe(10, 0);
+    }
+
+    void catchMe(int num1, int num2) {
+        //The following code results in an Exception.
+        try {
+            int result = num1 / num2;
+            System.out.println("The result is :" + result);
+        }catch (ArithmeticException arithmeticException){
+            System.out.println("Can't divine by 0");
+        }finally {
+            System.out.println("Thank you for using this program.");
+        }
+
+    }
+}
